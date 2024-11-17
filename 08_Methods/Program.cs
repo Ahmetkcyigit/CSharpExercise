7c9bfe78f2e92904a0e9af339e86e695b9976297
Ahmet Koçyiğit <kocyigittahmet@gmail.com>
2024-11-18 00:30:21 +0300

    08_Methods

---
+++ 08_Methods/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Void Metotlar
+
+            //Metotlar sonuna () alırlar ve solunda mor renk küp vardır
+            //Geriye değer döndürmeyen metotlar
+            //Customer - Listele, ekle, sil, güncelle
+            //Void
+
+            void CustomerList()
+            {
+                Console.WriteLine("Ali Yıldız");
+                Console.WriteLine("Ayşe Yıldız");
+                Console.WriteLine("Hakan Öztürk");
+                Console.WriteLine("Merve Çınar");
+            }
+
+            CustomerList();
+
+            #endregion
+            Console.Read();
+        }
+    }
+}
