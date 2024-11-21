52e3f82da99d45eb672b7938e49ae79914636406
Ahmet Koçyiğit <kocyigittahmet@gmail.com>
2024-11-22 00:02:21 +0300

    09_DatabaseProject

--- 09_DatabaseProject/Program.cs
+++ 09_DatabaseProject/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +13,41 @@ namespace _09_DatabaseProject
         static void Main(string[] args)
         {
 
+            //Ado.net
 
+            Console.WriteLine("***** C# Veri Tabanlı Ürün-Kategori Bilgi Sistemi *****");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            string tablenumber;
+
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("1-Kategoriler");
+            Console.WriteLine("2-Ürünler");
+            Console.WriteLine("3-Siparişler");
+            Console.WriteLine("4-Çıkış Yap");
+            Console.Write("Lütfen getirmek istediğiniz tablo numarasını giriniz: ");
+            tablenumber = Console.ReadLine();
+            Console.WriteLine("--------------------------------");
+
+            SqlConnection connection = new SqlConnection("Data Source=AhmetDestop\\SQLEXPRESS; initial Catalog=DbCSharpEgitim; integrated security= true;");
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("Select * From Category", connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+
+            connection.Close();
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                foreach(var item in row.ItemArray)
+                {
+                    Console.Write(item.ToString());
+                }
+                Console.WriteLine();
+            }
 
             Console.Read();
         }
